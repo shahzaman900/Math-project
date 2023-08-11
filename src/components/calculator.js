7ccba4d3eda9f53cc2b84cbda39318ba94fc7e68
sheezu900 <shahzamanlive2@gmail.com>
2023-08-11 16:49:49 +0500

    complete layout

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,7 +1,31 @@
 function Calculator() {
   return (
-    <div>
-      <h1>its working</h1>
+    <div className='calculator'>
+      <div className='display'>-</div>
+      <div className='row'>
+        <button>AC</button>
+        <button>+/-</button>
+        <button>%</button>
+        <button>/</button>
+      </div>
+      <div className='row'>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>x</button>
+      </div>
+      <div className='row'>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+      </div>
+       <div className='row'>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+      </div>
     </div>
   );
 }

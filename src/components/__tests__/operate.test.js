9b0a5ca692d35f897a129a332d28302968b83d68
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-08-16 11:13:08 +0100

    Add test for calculate.js

---
+++ src/components/__tests__/operate.test.js
@@ -0,0 +1,23 @@
+import operate from '../../logic/operate';
+
+describe('operate', () => {
+    test('adds two numbers correctly', () => {
+        const result = operate(2, 3, '+');
+        expect(result).toBe("5");
+    });
+
+    test('subtracts two numbers correctly', () => {
+        const result = operate(5, 3, '-');
+        expect(result).toBe("2");
+    });
+
+    test('multiplies two numbers correctly', () => {
+        const result = operate(4, 7, 'x');
+        expect(result).toBe("28");
+    });
+
+    test('divides two numbers correctly', () => {
+        const result = operate(10, 2, '÷');
+        expect(result).toBe("5");
+    });
+})

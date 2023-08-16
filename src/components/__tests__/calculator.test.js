85d734f7c86f4b0e198b200858c01078c4ffda9c
sheezu900 <shahzamanlive2@gmail.com>
2023-08-16 19:22:46 +0500

    complete testing

--- src/components/__tests__/calculator.test.js
+++ src/components/__tests__/calculator.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Calculator from '../calculator';
 
-test('renders learn react link', () => {
+test('Testing Calculator component', () => {
   render(<Calculator />);
   const linkElement = screen.getByText(/AC/i);
   expect(linkElement).toBeInTheDocument();
--- src/components/__tests__/getData.test.js
+++ src/components/__tests__/getData.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import Quote from '../getData';
 
-test('renders learn react link', () => {
+test('Testing Quote component', () => {
   render(<Quote />);
-  const linkElement = screen.getByText(/AC/i);
+  const linkElement = screen.getByText(/Loading/i);
   expect(linkElement).toBeInTheDocument();
 });

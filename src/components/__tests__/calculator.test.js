5ddbd71fca7ba34a896b1276dee22b1e3b795807
sheezu900 <shahzamanlive2@gmail.com>
2023-08-17 19:27:51 +0500

    add some styling

--- src/components/__tests__/calculator.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Calculator from '../calculator';
-
-test('Testing Calculator component', () => {
-  render(<Calculator />);
-  const linkElement = screen.getByText(/AC/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/components/__tests__/getData.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Quote from '../getData';
-
-test('Testing Quote component', () => {
-  render(<Quote />);
-  const linkElement = screen.getByText(/Loading/i);
-  expect(linkElement).toBeInTheDocument();
-});
